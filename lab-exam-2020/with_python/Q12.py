650f47ff18a3d2e929b38cf75431a63a6fcdef14
MasakiYoshizawa <cist.b218.m.yosizawa@gmail.com>
2020-11-30 11:39:22 +0900

    Q12,解答

--- lab-exam-2020/with_python/Q12.py
+++ lab-exam-2020/with_python/Q12.py
@@ -1,21 +1,30 @@
+import random
 from typing import List
 
 
 def generate_10_numbers_from_0_until_100() -> List[int]:
-
+    List = random.sample(range(0,100),k=10)
+    return List
 
 
 def print_list(numbers: List[int]):
-    pass
+    for i in numbers:
+        print(i)
 
 
 def sort_in_ascending_order(numbers: List[int]) -> List[int]:
-    pass
+    i=sorted(numbers)
+    return i
 
 
 def remove_content_which_number_is_under_50(numbers: List[int]) -> List[int]:
-    pass
-
+    i = [e for e in numbers if e>50]
+    return i
 
 if __name__ == '__main__':
-    pass
+    List = generate_10_numbers_from_0_until_100()
+    print_list(List)
+    print("--")
+    print_list(sort_in_ascending_order(List))
+    print('--')
+    print_list(remove_content_which_number_is_under_50(List))
